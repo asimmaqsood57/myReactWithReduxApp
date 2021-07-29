c3ae6f2dff48e1ad676571b5207d71276a07aa19
mariyaabid57 <mariyaabid1041@gmail.com>
2021-07-29 19:06:37 +0500

    updated

--- src/services/actions/Actions.js
+++ src/services/actions/Actions.js
@@ -1,1 +1,13 @@
-const addToCart = (data) => {};
+import Add_to_cart from "../Constants";
+export const addToCart = (data) => {
+  return {
+    type: addToCart,
+    data: data,
+  };
+};
+// export const removeToCart = (data) => {
+//   return {
+//     type: "Remove_to_cart",
+//     data: data,
+//   };
+// };
